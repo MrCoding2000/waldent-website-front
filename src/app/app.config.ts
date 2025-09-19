21cdda24a420b79f4ced4d7f0801aa55b770786f
MrCoding2000 <taha.softwareengineer2000@gmail.com>
2025-09-19 17:21:52 +0330

    add translate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,23 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
 import {jwtInterceptor} from './core/interceptores/jwt.interceptor';
 import {errorInterceptor} from './core/interceptores/error.interceptor';
+import {MessageService} from 'primeng/api';
+import {provideTranslateService} from '@ngx-translate/core';
+import {provideTranslateHttpLoader} from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideHttpClient(withInterceptors([jwtInterceptor, errorInterceptor]))
+    provideHttpClient(withInterceptors([jwtInterceptor, errorInterceptor])),
+    provideTranslateService({
+      lang: 'en',
+      fallbackLang: 'en',
+      loader: provideTranslateHttpLoader({ prefix: '/assets/i18n/', suffix: '.json' })
+    }),
+    provideHttpClient(withFetch()),
+    MessageService
   ]
 };
--- src/app/core/interceptores/error.interceptor.ts
+++ src/app/core/interceptores/error.interceptor.ts
@@ -2,14 +2,22 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { catchError, switchMap } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { inject } from '@angular/core';
+import { MessageService } from 'primeng/api';
 import {AuthService} from '../auth/service/auth.service';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const auth = inject(AuthService);
-
+  const messageService = inject(MessageService);
+  // TODO: Check Later
   return next(req).pipe(
     catchError(err => {
-      if (err.status === 401) {
+      let userMessage = '';
+
+      if (err.status === 0) {
+        userMessage = 'عدم ارتباط با سرور';
+      } else if (err.status === 401) {
+        userMessage = 'لطفاً مجدداً وارد شوید';
+        // تلاش برای Refresh Token
         return auth.refreshToken().pipe(
           switchMap(() => {
             const newToken = auth.getToken();
@@ -20,11 +28,20 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
           }),
           catchError(() => {
             auth.logout();
+            messageService.add({ severity: 'error', summary: 'خطا', detail: 'جلسه شما منقضی شد' });
             return throwError(() => new Error('Unauthorized'));
           })
         );
+      } else if (err.status >= 400 && err.status < 500) {
+        userMessage = err.error?.message || 'خطای سمت کاربر';
+      } else if (err.status >= 500) {
+        userMessage = 'خطای سرور';
       }
-      console.error(`HTTP Error: ${err.status} - ${err.message}`);
+
+      if (userMessage) {
+        messageService.add({ severity: 'error', summary: 'خطا', detail: userMessage });
+      }
+
       return throwError(() => err);
     })
   );
